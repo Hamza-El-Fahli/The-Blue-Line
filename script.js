47e76a55e43e9d2cf6d02534e8dd36979ccf0f72
Hamza-El-Fahli <Hamzaelfahli4@gmail.com>
2023-01-26 14:57:14 +0100

    setup the canvas

---
+++ script.js
@@ -0,0 +1,50 @@
+let canvas = document.querySelector("canvas");
+setupCanvas();
+
+
+
+const ctx = canvas.getContext('2d');
+
+function setupCanvas(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+};
+
+
+window.addEventListener("resize" , setupCanvas);
+
+const pointer = {
+    x : 0.5 * window.innerWidth,
+    y : 0.5 * window.innerHeight
+};
+
+function updateMousePosition (X,Y){
+    pointer.x = X;
+    pointer.y = Y;
+};
+
+window.addEventListener("click", e => {
+    updateMousePosition(e.pageX, e.pageY);
+});
+
+window.addEventListener("mousemove", e => {
+    updateMousePosition(e.pageX, e.pageY);
+});
+
+window.addEventListener("touchmove", e => {
+    updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
+});
+
+
+const  p = {x:0,y:0}
+function update(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    p.x = pointer.x ;
+    p.y = pointer.y ;
+    ctx.beginPath();
+    ctx.arc(p.x,p.y,5 , 0 , 2* Math.PI);
+    ctx.fill();
+    window.requestAnimationFrame(update)
+}
+
+update(0);
